2415b0ec992cf07e248ff70f1add0a4678130e29
arydz <arydz@sii.pl>
2020-10-09 23:53:24 +0200

    Fix sh script

--- tests/sitetest.sh
+++ tests/sitetest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if curl http://serve2:5000 | grep foo.txt; then
+if curl http://cloud-serve_serve_1:5000 | grep foo.txt; then
   echo "foo.txt exists.\nSite test passes.\n"
   exit 0
 else
